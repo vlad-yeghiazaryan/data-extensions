691b4cd52302bcd5a55209cc3eb5671ff074300e
vlad-yeghiazaryan <vladimir.y2000@gmail.com>
2023-12-27 18:58:00 +0400

    replica loss speed up

--- imputation/panel_imputation.py
+++ imputation/panel_imputation.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 
 # models
-from sklearn.linear_model import LinearRegression
 from sklearn.decomposition import PCA
+from scipy import linalg
 
 # missing data handling
 from sklearn.preprocessing import RobustScaler
@@ -13,9 +13,6 @@ from sklearn.preprocessing import RobustScaler
 # utils
 import warnings
 from tqdm.notebook import tqdm
-
-# runtime optimization
-from numba import jit
 
 class EmptySeriesImputer():
     """
@@ -36,12 +33,11 @@ class EmptySeriesImputer():
     - The DataFrame used in the impute method must have exactly 2 different index columns, usually representing 'country' and 'date'.
     - The 'fill_method' should be the name of one of these indices (e.g., 'country').
     """
-    def __init__(self, fill_method, scaler=None, model=None, recursive_imputation=True,
+    def __init__(self, fill_method, scaler=None, recursive_imputation=True,
                  aggregate_by='pca', alternative_priority_bias=False, partial_replica=False,
                  recursive_replication=False):
         self.fill_method = fill_method
         self.scaler = RobustScaler(quantile_range=(5.0, 95.0)) if type(scaler)==type(None) else scaler
-        self.model = LinearRegression(fit_intercept=False) if type(model)==type(None) else model
         self.alternative_priority_bias = alternative_priority_bias
         self.recursive_imputation = recursive_imputation
         self.aggregate_by = aggregate_by
@@ -59,8 +55,8 @@ class EmptySeriesImputer():
         return df.apply(ss)
 
     def fit_model(self, X, y):
-        res = self.model.fit(X, y)
-        return res.coef_
+        coef, _, rank, singular = linalg.lstsq(X, y)
+        return coef
 
     def calc_imputation_priority(self, df):
         missing_map = df.groupby(self.fill_method, group_keys=True).apply(lambda x: x.isna().any())
@@ -104,44 +100,44 @@ class EmptySeriesImputer():
             agg_pred = pred.mean(axis=1)
         return agg_pred
 
-    def aggregate_estimation(self, df_groups, group_names, group, columns, variable):
+    def aggregate_estimation(self, df_groups, group_index, variable_index):
         # setup exog
-        X = df_groups[np.where(group_names == group)[0][0]]
-        finite_cols = columns[np.isfinite(X).all(axis=0)]
-        finite_col_indices = np.where(np.isin(columns, finite_cols))[0]
+        X = df_groups[group_index]
+        finite_col_indices = np.where(np.isfinite(X).all(axis=0))[0]
         X = X[:, finite_col_indices]
 
         # estimate coefficients from all groups
         fitted_coefs = []
-        for group_name, group_item in zip(group_names, df_groups):
-            if (group_name!=group) and (group_item.size!=0):
+        for i in range(df_groups.shape[0]):
+            group_item = df_groups[i]
+            if (i!=group_index) and (group_item.size!=0):
                 X_group = group_item[:, finite_col_indices]
-                y_group = group_item[:, np.where(columns == variable)[0][0]]
-                finite_cols_group = finite_cols[np.isfinite(X_group).all(axis=0)]
-                finite_col_indices_group = np.where(np.isin(finite_cols, finite_cols_group))[0]
+                y_group = group_item[:, variable_index]
+                finite_col_indices_group = np.where(np.isfinite(X_group).all(axis=0))[0]
                 if (finite_col_indices_group.size != 0) and not np.isnan(y_group.sum()):
                     X_group = X_group[:, finite_col_indices_group]
                     some_group_coefs = self.fit_model(X_group, y_group)
-                    group_coefs = np.zeros_like(finite_cols, dtype=float)
-                    available_coefs = np.where(np.isin(finite_cols, finite_cols_group))[0]
-                    group_coefs[available_coefs] = some_group_coefs
+                    group_coefs = np.zeros_like(finite_col_indices, dtype=float)
+                    group_coefs[finite_col_indices_group] = some_group_coefs
                     fitted_coefs.append(group_coefs)
         fitted_coefs = np.array(fitted_coefs)
         fitted_preds = X @ fitted_coefs.T
-        pred = self.aggregate_predictions(fitted_preds)
-        return pred
+        return fitted_preds
 
-    def recursive_impute(self, df_scaled, df_filled):
+    def recursive_impute(self, df_scaled, df_filled, missing_priority):
         # Perform estimation using numpy
         index_1 = df_scaled.index.get_level_values(self.fill_method).to_numpy()
         columns = df_scaled.columns.to_numpy()
         df_array = df_scaled.to_numpy()
         group_names, group_id = np.unique(index_1, return_index=True)
         group_names = group_names[np.argsort(group_id)]
-        df_groups = np.split(df_array, len(group_names))
+        df_groups = np.array(np.split(df_array, len(group_names)))
         #  perform (recursive) imputation
-        for group, variable in self.missing_priority:
-            pred = self.aggregate_estimation(df_groups, group_names, group, columns, variable)
+        for group, variable in missing_priority:
+            group_index = np.where(group_names == group)[0][0]
+            variable_index = np.where(columns == variable)[0][0]
+            fitted_preds = self.aggregate_estimation(df_groups, group_index, variable_index)
+            pred = self.aggregate_predictions(fitted_preds)
             if self.recursive_imputation:
                 df_scaled.loc[group, variable] = pred
             df_filled.loc[group, variable] = pred
@@ -168,7 +164,7 @@ class EmptySeriesImputer():
         df_scaled['const'] = 1
 
         #  perform (recursive) imputation
-        df_filled = self.recursive_impute(df_scaled, df_filled)
+        df_filled = self.recursive_impute(df_scaled, df_filled, self.missing_priority)
         return self.package_df(df_filled, Flipped)
 
     def make_replica(self, df, original_missing_indices=None):
@@ -176,24 +172,50 @@ class EmptySeriesImputer():
         Create a replica matrix with imputed values by iteratively assuming that some series are empty and then filling them.
         """
         df, Flipped = self.setup_df(df)
-        df_replica = df.copy()
-        matrix_map = df.groupby(self.fill_method).sum().stack().index
+        a_miss = df.isna().all()
+        m_miss = df.isna().groupby(self.fill_method).any().all(axis=1)
+        df = df.drop(index=m_miss[m_miss].index, level=self.fill_method)
+        df = df.drop(columns=a_miss[a_miss].index)
+
+        # Standardize and add constant
+        df_scaled = df.groupby(self.fill_method, group_keys=False).apply(self.standardize_series)
+        df_replica = df_scaled.copy()
+        df_scaled['const'] = 1
+
+        # defining partial replica missing indices
         if self.partial_replica and type(original_missing_indices)==type(None):
             original_missing_indices = []
             warnings.warn("Please provide the original missing indices when partial replication is on.", UserWarning)
         elif not self.partial_replica:
             original_missing_indices = []
+        
+        # Perform estimation using numpy
+        index_1 = df_scaled.index.get_level_values(self.fill_method).to_numpy()
+        columns = df_scaled.columns.to_numpy()
+        df_array = df_scaled.to_numpy()
+        group_names, group_id = np.unique(index_1, return_index=True)
+        group_names = group_names[np.argsort(group_id)]
+        df_groups = np.array(np.split(df_array, len(group_names)))
+
+        # defining search space and iterating over all columns
+        matrix_map = df.groupby(self.fill_method).sum().stack().index
         for item in matrix_map:
-            df.loc[item] = np.NaN
-            replica_values = self.impute(df).loc[item].values
+            group, variable = item
+            gi = np.where(group_names == group)[0][0]
+            vi = np.where(columns == variable)[0][0]
+            df_groups[gi, :, vi] = np.NaN
+
+            #  perform imputation
+            fitted_preds = self.aggregate_estimation(df_groups, gi, vi)
+            replica_values = self.aggregate_predictions(fitted_preds)
             if not (item in original_missing_indices):
                 if self.recursive_replication:
-                    df.loc[item] = replica_values
+                    df_groups[gi, :, vi] = replica_values
                 else:
-                    df.loc[item] = df_replica.loc[item].values
+                    df_groups[gi, :, vi] = df_replica.loc[item].values
                 df_replica.loc[item] = replica_values
             else:
-                df.loc[item] = df_replica.loc[item].values
+                df_groups[gi, :, vi] = df_replica.loc[item].values
         return self.package_df(df_replica, Flipped)
 
     def fnorm(self, df, df_replica):
@@ -237,4 +259,3 @@ class EmptySeriesImputer():
             calibration_results.append(param)
         calibration_results = pd.DataFrame(calibration_results).sort_values('loss')
         return calibration_results
-
--- tests/test2.py
+++ tests/test2.py
@@ -12,8 +12,9 @@ from imputation.panel_imputation import EmptySeriesImputer
 panel_incomplete_dataset = pd.read_csv('../data/data_test2.csv', index_col=['country', 'date'])
 panel_incomplete_dataset.columns.name = 'variable'
 empty_imputer = EmptySeriesImputer('variable')
+panel_dataset_imputed = empty_imputer.impute(panel_incomplete_dataset)
 profiler = cProfile.Profile()
 profiler.enable()
-panel_dataset_imputed = empty_imputer.impute(panel_incomplete_dataset)
+loss = empty_imputer.replica_loss(panel_dataset_imputed)
 profiler.disable()
 profiler.dump_stats('profile_results.prof')
